fdd58aabf98f488d0d059e0643de12a786d00755
kalkidanh <kalkidanh89@gmail.com>
2023-05-29 20:48:23 +0300

    API status returned

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Imports app_views from api.v1.views"""
+from flask import jsonify
+from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route("/status", strict_slashes=False)
+def status():
+    """Return JSON status"""
+    return jsonify({'status': 'OK'})i
