be9762d5ebfe180d05aa9ba525a7e9398b3cbfde
kalkidanh <kalkidanh89@gmail.com>
2023-05-29 21:14:33 +0300

    typo fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,4 +8,4 @@ from api.v1.views import app_views
 @app_views.route("/status", strict_slashes=False)
 def status():
     """Return JSON status"""
-    return jsonify({'status': 'OK'})i
+    return jsonify({'status': 'OK'})
